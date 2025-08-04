06c5bc18ac0e5bc4b74453ae235b25025785d5e6
Gautam Maurya <gautammaurya1299@gmail.com>
2025-08-04 21:12:49 +0530

    Add oops_1.py with class definitions and display functionality

---
+++ Anonymus_files/oops_1.py
@@ -0,0 +1,20 @@
+class x:
+    def __init__(self):
+        self._name = 1
+        self.age = 5
+        self.__private = 3
+        
+    def display(self):
+        print(f"Name: {self._name}, Age: {self.age} and Private: {self.__private}")
+        
+        
+class y(x):
+    def __init__(self):
+        super().__init__()
+        self._name = 2
+        self.age = 10
+        self.__private = 6
+
+   
+obj= y()
+obj.display()  
