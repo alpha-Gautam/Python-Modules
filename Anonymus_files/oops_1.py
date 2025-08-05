08bfed0b80eba870c830b9449274d37155058210
Gautam Maurya <gautammaurya1299@gmail.com>
2025-08-05 16:55:02 +0530

    Refactor oops_1.py: Update class x to use private variables correctly and adjust class y initialization

--- Anonymus_files/oops_1.py
+++ Anonymus_files/oops_1.py
@@ -1,17 +1,21 @@
 class x:
     def __init__(self):
+        
         self._name = 1
-        self.age = 5
-        self.__private = 3
+        # self.age = 5
+        # self.__private = 3
         
     def display(self):
+        self._name = 2
+        self.age = 5
+        self.__private = 3
         print(f"Name: {self._name}, Age: {self.age} and Private: {self.__private}")
         
         
 class y(x):
     def __init__(self):
         super().__init__()
-        self._name = 2
+        self._name = 3
         self.age = 10
         self.__private = 6
 
