f174ddf1fc5805f38f3a386bfc1fa97f19aa4691
Gautam Maurya <gautammaurya1299@gmail.com>
2025-08-06 20:50:30 +0530

    Refactor oops_1.py: Correct initialization of class variables in classes x and y

--- Anonymus_files/oops_1.py
+++ Anonymus_files/oops_1.py
@@ -2,21 +2,21 @@ class x:
     def __init__(self):
         
         self._name = 1
-        # self.age = 5
-        # self.__private = 3
+        self.age = 2
+        self.__private = 3
         
     def display(self):
-        self._name = 2
-        self.age = 5
-        self.__private = 3
+        # self._name = 2
+        # self.age = 5
+        # self.__private = 3
         print(f"Name: {self._name}, Age: {self.age} and Private: {self.__private}")
         
         
 class y(x):
     def __init__(self):
         super().__init__()
-        self._name = 3
-        self.age = 10
+        self._name = 4
+        self.age = 5
         self.__private = 6
 
    
